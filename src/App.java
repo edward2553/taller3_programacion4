a7c31f20123959d3bc3ad9db0d312f226c6bfaf6
edward2553 <edwaramor9@gmail.com>
2022-06-13 21:06:24 -0500

    se termina la parte visual del formulario

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class App extends JFrame {
+public class App {
     private JPanel mainPanel;
     private JButton submitButton;
     private JTextField nameField;
@@ -8,17 +12,187 @@ public class App extends JFrame {
     private JLabel registerLabel;
     private JLabel nameLabel;
     private JLabel lastnamesLabels;
-    private JComboBox comboBox1;
-    private JTextField textField1;
-    private JTextField textField3;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JTextField textField2;
-    private JTextField textField4;
-    private JPasswordField passwordField1;
-    private JPasswordField passwordField2;
-    private JCheckBox autorizoElTratamientoDeCheckBox;
-    private JCheckBox aceptoLosTerminosYCheckBox;
+    private JComboBox idField;
+    private JTextField idNumberField;
+    private JTextField phone;
+    private JComboBox nationalityField;
+    private JComboBox placeLiveField;
+    private JTextField emailField;
+    private JTextField confirmEmailField;
+    private JPasswordField passwordField;
+    private JPasswordField confirmPasswordField;
+    private JCheckBox checkData;
+    private JCheckBox checkTermAndConditions;
+    private JLabel nameError;
+    private JLabel lastnameError;
+    private JLabel idError;
+    private JLabel idNumberError;
+    private JLabel placeLiveError;
+    private JLabel phoneError;
+    private JLabel nationalityError;
+    private JLabel emailError;
+    private JLabel confirmEmailError;
+    private JLabel passwordError;
+    private JLabel confirmPasswrodError;
+
+    private Boolean hasError = false;
+
+    public App() {
+        String selectLabel = "Seleccione";
+        String ids[] = {selectLabel, "Cédula de ciudadanía", "Cédula de extranjería", "Pasaporte"};
+        idField.addItem(ids[0]);
+        idField.addItem(ids[1]);
+        idField.addItem(ids[2]);
+        idField.addItem(ids[3]);
+
+        String places[] = {selectLabel,"Amazonas", "Antioquia", "Arauca", "Atlántico", "Bogotá", "Bolívar", "Boyacá", "Caldas", "Caquetá", " Chocó"};
+        placeLiveField.addItem(places[0]);
+        placeLiveField.addItem(places[1]);
+        placeLiveField.addItem(places[2]);
+        placeLiveField.addItem(places[3]);
+        placeLiveField.addItem(places[4]);
+        placeLiveField.addItem(places[5]);
+        placeLiveField.addItem(places[6]);
+        placeLiveField.addItem(places[7]);
+        placeLiveField.addItem(places[8]);
+        placeLiveField.addItem(places[9]);
+        placeLiveField.addItem(places[10]);
+
+        nationalityField.addItem(selectLabel);
+        nationalityField.addItem("Colombiano (a)");
+        nationalityField.addItem("Otro");
+
+        nameField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+//                Minimo dos digitos, no incluir numeros ni caracteres especiales
+                if (!nameField.getText().matches("[a-zA-ZÀ-úüñ\\s]{2,}")) {
+                    hasError = true;
+                    nameError.setText("El campo debe contener mínimo dos digitos y no incluir números ni caracteres especiales");
+                } else {
+                    hasError = false;
+                    nameError.setText("");
+                }
+            }
+        });
+        lastnameField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!lastnameField.getText().matches("[a-zA-ZÀ-úüñ\\s]{2,}")) {
+                    hasError = true;
+                    lastnameError.setText("El campo debe contener mínimo dos digitos y no incluir números ni caracteres especiales");
+                } else {
+                    hasError = false;
+                    lastnameError.setText("");
+                }
+            }
+        });
+        idNumberField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!idNumberField.getText().matches("^[0-9]{6,10}$")) {
+                    hasError = true;
+                    idNumberError.setText("El campo debe contener entre 6 y 10 digitos e incluir solo números");
+                } else {
+                    hasError = false;
+                    idNumberError.setText("");
+                }
+            }
+        });
+        phone.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!phone.getText().matches("^[0-9]{10,}$")) {
+                    hasError = true;
+                    phoneError.setText("El campo debe contener minimo 10 digitos e incluir solo números");
+                } else {
+                    hasError = false;
+                    phoneError.setText("");
+                }
+            }
+        });
+        emailField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!emailField.getText().matches("^\\S+@\\S+\\.\\S+$")) {
+                    hasError = true;
+                    emailError.setText("Verificar el correo elecronico ingresado");
+                } else {
+                    hasError = false;
+                    emailError.setText("");
+                }
+            }
+        });
+        confirmEmailField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                System.out.println(emailField.getText());
+                System.out.println(confirmEmailField.getText());
+                if (!confirmEmailField.getText().matches("^\\S+@\\S+\\.\\S+$")) {
+                    hasError = true;
+                    confirmEmailError.setText("Verificar el correo elecronico ingresado");
+                } else {
+                    hasError = false;
+                    confirmEmailError.setText("");
+                }
+            }
+        });
+        passwordField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                System.out.println(String.valueOf(passwordField.getPassword()));
+                if (!String.valueOf(passwordField.getPassword())
+                        .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$")
+                    ) {
+                    hasError = true;
+                    passwordError.setText("La contraseña debe tener al menos 8 caracteres, una minuscula, una mayuscula y un número");
+                } else {
+                    hasError = false;
+                    passwordError.setText("");
+                }
+            }
+        });
+        confirmPasswordField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (!String.valueOf(confirmPasswordField.getPassword())
+                        .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$")
+                ) {
+                    hasError = true;
+                    confirmPasswrodError.setText("La contraseña debe tener al menos 8 caracteres, una minuscula, una mayuscula y un número");
+                } else {
+                    hasError = false;
+                    confirmPasswrodError.setText("");
+                }
+            }
+        });
+        confirmPasswordField.addKeyListener(new KeyAdapter() {
+        });
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                if (String.valueOf(confirmPasswordField.getPassword()) != String.valueOf(passwordField.getPassword())){
+//                    hasError = true;
+//                    confirmPasswrodError.setText("Las contraseñas deben coincidir");
+//                }
+//                if (emailField.getText() != confirmEmailField.getText()) {
+//                    hasError = true;
+//                    confirmEmailError.setText("Los correos no coinciden");
+//                }
+                if (hasError) {
+                    JOptionPane.showMessageDialog(null,"Revisa el Formulario con los errores");
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("App");
